538ae0755e74b4c91c20c1657ec1c81b4bb20620
Rober2092 <90662615+Rober2092@users.noreply.github.com>
2022-07-16 12:04:59 -0700

    update files and code

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by hugo Alvarado on 7/15/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
